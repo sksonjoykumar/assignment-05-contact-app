9ed4e5772fec7ea06ebb65c16b42e0ef593bca75
sksonjoykumar <sksonjoykumar111@gmail.com>
2026-01-17 12:59:12 +0600

    some feature added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <main className="sticky top-0 border-b border-gray-100 py-4 shadow-sm">
+      <main className="sticky top-0 border-b border-gray-100 bg-white py-4 shadow-sm">
         <div className="mx-auto max-w-312.5 px-4 md:px-20">
           <Link
             to={"/"}
